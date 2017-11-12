fc9d670bd41c018462734366abb4425373505d92
Pawel Lorek <palorek@gmail.com>
2017-11-12 17:54:10 +0100

    Improve score counting (your pairs)

--- game/players/score_calculator.py
+++ game/players/score_calculator.py
@@ -4,6 +4,62 @@ import numpy as np
 class ScoreCalculator:
   def __init__(self, board):
     self.board = board
+    self.current_score = 0
 
-  def score(self):
-    return 0
+  def score_for_player(self, player):
+    self.add_players_pairs(player)
+    self.substract_oponents_pairs(player)
+    self.add_wining_condition(player)
+    self.substract_loosing_condition(player)
+    return self.current_score
+
+  def add_players_pairs(self, player):
+    pairs_count = self.find_pairs_count(player)
+    self.current_score += pairs_count
+
+  def substract_oponents_pairs(self, player):
+    pass
+
+  def add_wining_condition(self, player):
+    pass
+
+  def substract_loosing_condition(self, player):
+    pass
+
+
+  def find_pairs_count(self, player):
+    pairs_count = 0
+    for i in range(0, self.board.shape[0]):                     # rows
+      if self.two_in_a_row(self.board[i, :], player):
+        # print('Found two in row ', i)
+        y = np.where(self.board[i, :] == 0)[0][0]
+        pairs_count += 1
+        # return [i, y]
+
+    for i in range(0, self.board.shape[1]):                     # columns
+      if self.two_in_a_row(self.board[:, i], player):
+        # print('Found two in col ', i)
+        x = np.where(self.board[:, i] == 0)[0][0]
+        pairs_count += 1
+        # return [x, i]
+
+    if self.two_in_a_row(np.diag(self.board), player): # diagonal
+      # print('Found two on diag ')
+      diag_pos = np.where(np.diag(self.board) == 0)[0][0]
+      pairs_count += 1
+      # return [diag_pos, diag_pos]
+
+    if self.two_in_a_row(np.diag(np.flipud(self.board)), player): # anty-diagonal
+      # print('Found two on anty-diag ')
+      anty_diag_pos = np.where(np.diag(np.flipud(self.board)) == 0)[0][0]
+      pairs_count += 1
+      # return [board.shape[0] - 1 - anty_diag_pos, anty_diag_pos]
+
+    return pairs_count
+
+
+  def two_in_a_row(self, row, player):
+    row = row.copy()
+    row.sort()
+    if row[0] == 0 and row[1] == player and row[2] == player: return True
+    return False
--- tests/score_calculator_test.py
+++ tests/score_calculator_test.py
@@ -3,11 +3,33 @@ import numpy as np
 from game.players.score_calculator import ScoreCalculator
 
 def empty_board():
-  np.zeros((3, 3))
+  return np.zeros((3, 3))
+
 class ScoreCalculatorTest(unittest.TestCase):
 
   def test_constructor(self):
     self.assertEqual([], ScoreCalculator([]).board)
 
   def test_score_for_empty_board(self):
-    self.assertEqual(0, ScoreCalculator(empty_board()).score())
+    self.assertEqual(0, ScoreCalculator(empty_board()).score_for_player(1))
+    self.assertEqual(0, ScoreCalculator(empty_board()).score_for_player(2))
+
+  def test_score_counts_single_pair(self):
+    board = empty_board()
+    board[0,0] = 1
+    board[0,1] = 1
+    self.assertEqual(1, ScoreCalculator(board).score_for_player(1))
+
+  def test_score_counts_multiple_pairs(self):
+    board = empty_board()
+    board[0,0] = 1
+    board[0,1] = 1
+    board[1,1] = 1
+    self.assertEqual(3, ScoreCalculator(board).score_for_player(1))
+
+  def test_score_counts_properly_multiple_selections_without_pair(self):
+    # no pairs 2 selections, but on "check kinght move" positions
+    board = empty_board()
+    board[0,0] = 1
+    board[1,2] = 1
+    self.assertEqual(0, ScoreCalculator(board).score_for_player(1))
